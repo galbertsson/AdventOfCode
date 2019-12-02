6f7cdcdfb14abf055ff5f1a7dcf5efd41674c116
galbertsson <gustav.albertsson@hotmail.com>
2019-12-02 17:41:19 +0100

    Added step2, cleaned up code

--- 2dec/2dec.js
+++ 2dec/2dec.js
@@ -1,40 +1,69 @@
-const run = (program) => {
+const program = require('./program.json')
+
+const opCodes = {
+    1: {
+        parameters: 3, applyOp: (parameters, memory) => {
+            memory[parameters[2]] = memory[parameters[0]] + memory[parameters[1]];
+            return memory
+        }
+    },
+    2: {
+        parameters: 3, applyOp: (parameters, memory) => {
+            memory[parameters[2]] = memory[parameters[0]] * memory[parameters[1]]
+            return memory
+        }
+    },
+    99: { parameters: 0, applyOp: (parameters, memory) => false },
+}
+
+const run = (program, opCodes) => {
+    let programCopy = Object.assign([], program)
+
     let pc = 0
-    while (pc < program.length) {
-        const {addr, value} = doOp(program.slice(pc, pc+4), program)
-        if (addr !== undefined && value !== undefined) {
-            program[addr] = value
+    while (pc < programCopy.length) {
+        const opCode = programCopy[pc]
+        pc++
+
+        const { parameters, applyOp } = opCodes[opCode]
+
+        const mem = applyOp(programCopy.slice(pc, pc + parameters), programCopy)
+
+        if (mem) {
+            programCopy = mem
         } else {
-            return program;
+            return programCopy
         }
-        if (program[0] === 19690720) {
-            console.log(program[1])
-            console.log(program[2])
-            return;
-        }
-        pc += 4
-    }
-}
 
-const doOp = (op, program) => {
-
-    if (op[0] === 1) {
-        return {addr: op[3], value: program[op[1]]+program[op[2]]}
-    } else if (op[0] === 2) {
-        return {addr: op[3], value: program[op[1]]*program[op[2]]}
-    } else if (op[0] === 99){
-        return {}
+        pc = pc + parameters
     }
 
-    console.log('No Op code')
+    return programCopy
 }
 
+const permuteProgram = (program, opCodes) => {
+    for (let i = 0; i <= 99; i++) {
+        for (let j = 0; j <= 99; j++) {
+            const programCopy = Object.assign([], program)
+            programCopy[2] = j
+            programCopy[1] = i
+            const result = run(programCopy, opCodes)
+            if (result[0] === 19690720) {
+                return programCopy
+            }
+        }
+    }
+}
 
 console.log('Should all be true')
-console.log(JSON.stringify(run([1,0,0,0,99])) === JSON.stringify([2,0,0,0,99]))
-console.log(JSON.stringify(run([2,3,0,3,99])) === JSON.stringify([2,3,0,6,99]))
-console.log(JSON.stringify(run([2,4,4,5,99,0])) === JSON.stringify([2,4,4,5,99,9801]))
-console.log(JSON.stringify(run([1,1,1,4,99,5,6,0,99])) === JSON.stringify([30,1,1,4,2,5,6,0,99]))
+console.log(JSON.stringify(run([1, 0, 0, 0, 99], opCodes)) === JSON.stringify([2, 0, 0, 0, 99]))
+console.log(JSON.stringify(run([2, 3, 0, 3, 99], opCodes)) === JSON.stringify([2, 3, 0, 6, 99]))
+console.log(JSON.stringify(run([2, 4, 4, 5, 99, 0], opCodes)) === JSON.stringify([2, 4, 4, 5, 99, 9801]))
+console.log(JSON.stringify(run([1, 1, 1, 4, 99, 5, 6, 0, 99], opCodes)) === JSON.stringify([30, 1, 1, 4, 2, 5, 6, 0, 99]))
 
 console.log('Part 1:')
-console.log(run([1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,13,19,23,2,23,9,27,1,6,27,31,2,10,31,35,1,6,35,39,2,9,39,43,1,5,43,47,2,47,13,51,2,51,10,55,1,55,5,59,1,59,9,63,1,63,9,67,2,6,67,71,1,5,71,75,1,75,6,79,1,6,79,83,1,83,9,87,2,87,10,91,2,91,10,95,1,95,5,99,1,99,13,103,2,103,9,107,1,6,107,111,1,111,5,115,1,115,2,119,1,5,119,0,99,2,0,14,0]))
+console.log(run(program, opCodes)[0])
+
+console.log('Part 2:')
+const resultProgram = permuteProgram(program, opCodes)
+console.log('Noun', resultProgram[1])
+console.log('Verb', resultProgram[2])
