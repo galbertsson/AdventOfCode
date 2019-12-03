986ca5ed7d3bb60449f98d087101c8ead26469a2
galbertsson <gustav.albertsson@hotmail.com>
2019-12-03 20:40:35 +0100

    Added kinda hacky step 2 solution

--- 3dec/3dec.js
+++ 3dec/3dec.js
@@ -85,9 +85,39 @@ const computeClosestPoint = (firstCablePath, secondCablePath) => {
     return distanceToCentralPort(closestPoint)
 }
 
+const totalWireLength = (firstPath, secondPath, intersection) => {
+    const firstPathLength = firstPath.findIndex(point => point.x === intersection.x && point.y === intersection.y)
+    const secondPathLength = secondPath.findIndex(point => point.x === intersection.x && point.y === intersection.y)
+
+    return firstPathLength+secondPathLength
+}
+
+const computeBestPoint = (firstCablePath, secondCablePath) => {
+    const firstParsedInput = buildCompletePath(parseInput(firstCablePath))
+    const secondParsedInput = buildCompletePath(parseInput(secondCablePath))
+
+    const intersections = findIntersections(firstParsedInput, secondParsedInput)
+
+    let distance
+    for (let intersection of intersections) {
+        const tmpDistance = totalWireLength(firstParsedInput, secondParsedInput, intersection)
+        if (distance === undefined || tmpDistance < distance) {
+            distance = tmpDistance
+        }
+    }
+
+    return distance
+}
+
 console.log('Should be true')
 console.log(computeClosestPoint(["R75","D30","R83","U83","L12","D49","R71","U7","L72"], ["U62","R66","U55","R34","D71","R55","D58","R83"]) === 159)
 console.log(computeClosestPoint(["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"], ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]) === 135)
 
 console.log('Step1: ')
 console.log(computeClosestPoint(path1, path2))
+
+console.log('Step2: ')
+console.log('should be true')
+console.log(computeBestPoint(["R75","D30","R83","U83","L12","D49","R71","U7","L72"], ["U62","R66","U55","R34","D71","R55","D58","R83"]) === 610)
+console.log(computeBestPoint(["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"], ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]) === 410)
+console.log(computeBestPoint(path1, path2))
